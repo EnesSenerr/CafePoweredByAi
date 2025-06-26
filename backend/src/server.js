ad969d0afe81b2ea30cc97185093efae5a9ee0af
ENES ŞENER <enessener0033@outlook.com>
2025-06-27 02:04:47 +0300

    add vercel domain

--- backend/src/server.js
+++ backend/src/server.js
@@ -19,7 +19,8 @@ app.use(cors({
     'http://127.0.0.1:3001',
     'http://192.168.1.102:3000',
     'http://192.168.1.102:3001',
-    'http://192.168.1.102:5000'
+    'http://192.168.1.102:5000',
+    'https://cafe-powered-by-ai-wt2d.vercel.app'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
